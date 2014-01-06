f9e189665c3c151ba68b3bb30837bf322663ff3a
Stephan Kleine <stephan.kleine@gmail.com>
2014-01-06 03:33:06 +0100

    update thread dump servlet path

--- sling-metrics-bundle/src/main/java/com/github/digital_wonderland/sling_metrics/servlets/ThreadDumpServlet.java
+++ sling-metrics-bundle/src/main/java/com/github/digital_wonderland/sling_metrics/servlets/ThreadDumpServlet.java
@@ -2,7 +2,7 @@ package com.github.digital_wonderland.sling_metrics.servlets;
 
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 
-@SlingServlet(paths = "/bin/sling-metrics/threaddump", methods = "GET")
+@SlingServlet(paths = "/bin/sling-metrics/threads", methods = "GET")
 public class ThreadDumpServlet extends com.codahale.metrics.servlets.ThreadDumpServlet {
 
 }
