219869a8eae5725e96348f74e69ad61c1c0c85e1
Stephan Kleine <stephan.kleine@gmail.com>
2014-01-06 03:29:27 +0100

    add thread dump servlet

---
+++ sling-metrics-bundle/src/main/java/com/github/digital_wonderland/sling_metrics/servlets/ThreadDumpServlet.java
@@ -0,0 +1,8 @@
+package com.github.digital_wonderland.sling_metrics.servlets;
+
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+
+@SlingServlet(paths = "/bin/sling-metrics/threaddump", methods = "GET")
+public class ThreadDumpServlet extends com.codahale.metrics.servlets.ThreadDumpServlet {
+
+}
