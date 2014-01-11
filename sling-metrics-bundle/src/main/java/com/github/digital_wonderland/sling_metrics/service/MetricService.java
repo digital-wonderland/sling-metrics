ab1dbb42784a6d4c0bf0724dab1287583c01e72e
Stephan Kleine <stephan.kleine@gmail.com>
2014-01-11 23:21:51 +0100

    add javadoc

--- sling-metrics-bundle/src/main/java/com/github/digital_wonderland/sling_metrics/service/MetricService.java
+++ sling-metrics-bundle/src/main/java/com/github/digital_wonderland/sling_metrics/service/MetricService.java
@@ -63,21 +63,74 @@ public class MetricService {
 
     public String getMetricPrefix() { return metricPrefix; }
 
+    /**
+     * Concatenates elements to form a dotted name, eliding any null values or empty strings.
+     *
+     * @param name     the first element of the name
+     * @param names    the remaining elements of the name
+     * @return {@code name} and {@code names} concatenated by periods
+     * @see com.codahale.metrics.MetricRegistry#name
+     */
     public String name(String name, String... names) {
         return com.codahale.metrics.MetricRegistry.name(String.format("%s.%s", metricPrefix, name), names);
     }
 
+    /**
+     * Given a {@link Metric}, registers it under the given name.
+     *
+     * @param name   the name of the metric
+     * @param metric the metric
+     * @param <T>    the type of the metric
+     * @return {@code metric}
+     * @throws IllegalArgumentException if the name is already registered
+     * @see com.codahale.metrics.MetricRegistry#register
+     */
     public <T extends Metric> T register(String name, T metric) throws IllegalArgumentException {
         return registry.register(name(name), metric);
     }
 
+    /**
+     * Removes the metric with the given name.
+     *
+     * @param name the name of the metric
+     * @return whether or not the metric was removed
+     * @see com.codahale.metrics.MetricRegistry#remove
+     */
     public boolean remove(String name) { return registry.remove(name(name)); }
 
+    /**
+     * Creates a new {@link Meter} and registers it under the given name.
+     *
+     * @param name the name of the metric
+     * @return a new {@link Meter}
+     * @see com.codahale.metrics.MetricRegistry#meter
+     */
     public Meter meter(String name) { return registry.meter(name(name)); }
 
+    /**
+     * Creates a new {@link Histogram} and registers it under the given name.
+     *
+     * @param name the name of the metric
+     * @return a new {@link Histogram}
+     * @see com.codahale.metrics.MetricRegistry#histogram
+     */
     public Histogram histogram(String name) { return registry.histogram(name(name)); }
 
+    /**
+     * Creates a new {@link Counter} and registers it under the given name.
+     *
+     * @param name the name of the metric
+     * @return a new {@link Counter}
+     * @see com.codahale.metrics.MetricRegistry#counter
+     */
     public Counter counter(String name) { return registry.counter(name(name)); }
 
+    /**
+     * Creates a new {@link Timer} and registers it under the given name.
+     *
+     * @param name the name of the metric
+     * @return a new {@link Timer}
+     * @see com.codahale.metrics.MetricRegistry#timer
+     */
     public Timer timer(String name) { return registry.timer(name(name)); }
 }
